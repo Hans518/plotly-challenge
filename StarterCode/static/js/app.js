111bef50b23a53301190827c836b6c18872d6a39
Hans Engelbrecht <hengelbrecht@godaddy.com>
2020-06-10 19:37:28 -0700

    initial commit after saving

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -116,27 +116,29 @@ function getSampless(val) {
 }
 
 function bubbleChart() {
-  var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 11, 12, 13],
-    mode: 'markers',
-    marker: {
-      color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-      opacity: [1, 0.8, 0.6, 0.4],
-      size: [40, 60, 80, 100]
-    }
-  };
+
   
-  var data = [trace1];
-  
-  var layout = {
-    title: 'Marker Size and Color',
-    showlegend: false,
-    height: 600,
-    width: 600
-  };
-  
-  Plotly.newPlot("bubble", data, layout);
+    var trace1 = {
+      x: [1, 2, 3, 4],
+      y: [10, 11, 12, 13],
+      mode: 'markers',
+      marker: {
+        color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+        opacity: [1, 0.8, 0.6, 0.4],
+        size: [40, 60, 80, 100]
+      }
+    };
+    
+    var data = [trace1];
+    
+    var layout = {
+      title: 'Marker Size and Color',
+      showlegend: false,
+      height: 600,
+      width: 600
+    };
+    
+    Plotly.newPlot("bubble", data, layout);
 }  
 
 
